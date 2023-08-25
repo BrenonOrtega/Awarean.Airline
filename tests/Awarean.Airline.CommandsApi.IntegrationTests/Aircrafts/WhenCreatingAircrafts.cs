f3d057bde131444c3324b7de5509b8e54f77e74e
BrenonOrtega <brenon.ortega@hotmail.com>
2023-08-25 08:01:19 +0200

    Adds first tests to Aircraft creation controller

---
+++ tests/Awarean.Airline.CommandsApi.IntegrationTests/Aircrafts/WhenCreatingAircrafts.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Flurl.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Net;
+
+namespace Awarean.Airline.CommandsApi.UnitTests.Aircrafts;
+
+public class WhenCreatingAircrafts
+{
+    [Fact]
+    public async Task Should_Validate_Input()
+    {
+        var testServer = new WebApplicationFactory<Awarean.Airline.CommandsApi.Program>();
+
+        using var client = new FlurlClient(testServer.CreateClient());
+
+        var response = await "aircrafts".WithClient(client)
+            .PostJsonAsync(new
+            {
+                model = "Boeing-747 MAX",
+                actualParkingLocation = "BCN",
+                type = "Cargo"
+            });
+
+        var content = await response.GetJsonAsync();
+
+        Assert.True(Guid.TryParse(content.authenticationCode, out Guid _));
+        response.StatusCode.Should().Be((int)HttpStatusCode.Accepted);
+    }
+}
