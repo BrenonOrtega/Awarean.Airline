8a9d6cf68df7e9510267beee1ff1de730f4d3c9a
BrenonOrtega <brenon.ortega@hotmail.com>
2023-08-25 07:56:36 +0200

    Adds string extensions for validations of null and empty strings.

---
+++ src/Awarean.Airline.Extensions/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Awarean.Airline.Extensions;
+
+public static class StringExtensions
+{
+    public static string ThrowIfNullOrEmpty(this string @string, string argName)
+    {
+        if (string.IsNullOrEmpty(@string))
+            throw new ArgumentException($"'{argName}' cannot be null or empty.", argName);
+
+        return @string;
+    }
+}
